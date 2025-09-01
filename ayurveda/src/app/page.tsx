2d8bab56e9dc6b6b265f8aa70fd9b1a41276c6a1
MishraShardendu22 <shardendumishrabrave@gmail.com>
2025-09-01 15:11:08 +0530

    feat: initialised repo
    Signed-off-by: MishraShardendu22 <shardendumishrabrave@gmail.com>

---
+++ ayurveda/src/app/page.tsx
@@ -0,0 +1,10 @@
+const page = () => {
+  return (
+    <div>
+      <h1>Ayurveda</h1>
+      <p>Welcome to the Ayurveda page!</p>
+    </div>
+  )
+}
+
+export default page
---
+++ internship/src/app/page.tsx
@@ -0,0 +1,10 @@
+const page = () => {
+  return (
+    <div>
+      <h1>Internship</h1>
+      <p>Welcome to the Internship page!</p>
+    </div>
+  )
+}
+
+export default page
