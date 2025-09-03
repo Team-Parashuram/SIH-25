4c69a6869a87bcf57e53843a86970be15f25cd83
ANAS727189 <anas23khan083@gmail.com>
2025-09-03 18:59:41 +0530

    feat: configured the server setup for namaste

---
+++ namaste/server/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { Request, Response } from "express";
+const app = express();
+const port  = 3000;
+
+app.get('/health', (req: Request, res: Response) => {
+    res.send('Hello World!, Health check done. Everything is working as expected!');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
