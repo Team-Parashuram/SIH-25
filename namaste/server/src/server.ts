b9d553c8bb2581372aef04d9635bc4b278d105d5
ANAS727189 <anas23khan083@gmail.com>
2025-09-03 19:15:05 +0530

    chore: added who-ayurveda pdf link in txt file for reference

--- namaste/server/src/server.ts
+++ namaste/server/src/server.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import { Request, Response } from "express";
+import express from 'express';
+import { Request, Response } from 'express';
 const app = express();
-const port  = 3000;
+const port = 3000;
 
 app.get('/health', (req: Request, res: Response) => {
-    res.send('Hello World!, Health check done. Everything is working as expected!');
+  res.send(
+    'Hello World!, Health check done. Everything is working as expected!'
+  );
 });
 
-
-
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
